178d29d61b0d7717e3798afc77f8568ac9e73aee
xin.li <xin.li@daocloud.io>
2023-10-15 23:38:16 +0800

    add client-go practice
    Signed-off-by: xin.li <xin.li@daocloud.io>

---
+++ kubernetes-operator/client-go-demo/04-client-go-practice/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	"github.com/my-git9/kubernetes-dev/client-go-demo/04-client-go-practice/pkg"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	// 1. create config
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		// if no kubeconfig, by token
+		inCluserConfig, err := rest.InClusterConfig()
+		if err != nil {
+			log.Fatalln("cat not get config")
+		}
+		config = inCluserConfig
+	}
+
+	// 2. create clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalln("can't create client")
+	}
+
+	// 3. create informer
+	factory := informers.NewSharedInformerFactory(clientset, 0)
+	serviceInformer := factory.Core().V1().Services()
+	ingressInformer := factory.Networking().V1().Ingresses()
+
+	// 4. add informer handler
+	controller := pkg.NewController(clientset, serviceInformer, ingressInformer)
+
+	stopCh := make(chan struct{})
+	// 5. start informer
+	factory.Start(stopCh)
+	// 等待数据同步完成后
+	factory.WaitForCacheSync(stopCh)
+	// 启动 controller
+	controller.Run(stopCh)
+
+}
---
+++ kubernetes-operator/client-go-demo/04-client-go-practice/pkg/controller.go
@@ -0,0 +1,200 @@
+package pkg
+
+import (
+	"context"
+	"log"
+	"reflect"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	informer "k8s.io/client-go/informers/core/v1"
+	networkinginformer "k8s.io/client-go/informers/networking/v1"
+	"k8s.io/client-go/kubernetes"
+	coreLister "k8s.io/client-go/listers/core/v1"
+	v1 "k8s.io/client-go/listers/networking/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
+
+const (
+	workNum  = 5
+	maxRetry = 5
+)
+
+type controller struct {
+	client        kubernetes.Interface
+	ingressLister v1.IngressLister
+	serviceLister coreLister.ServiceLister
+	queue         workqueue.RateLimitingInterface
+}
+
+func (c *controller) addService(obj interface{}) {
+	c.enqueue(obj)
+}
+
+func (c *controller) updateService(oldobj interface{}, newobj interface{}) {
+	if reflect.DeepEqual(oldobj, newobj) {
+		return
+	}
+	c.enqueue(newobj)
+}
+
+func (c *controller) deleteIngress(obj interface{}) {
+	ingress := obj.(*networkingv1.Ingress)
+	ownerReference := metav1.GetControllerOf(ingress)
+	if ownerReference == nil {
+		return
+	}
+	if ownerReference.Kind != "Service" {
+		return
+	}
+
+	c.queue.Add(ingress.Namespace + "/" + ingress.Name)
+}
+
+func (c *controller) enqueue(obj interface{}) {
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	c.queue.Add(key)
+}
+
+func (c *controller) Run(stopCh chan struct{}) {
+	for i := 0; i < workNum; i++ {
+		go wait.Until(c.worker, time.Minute, stopCh)
+	}
+
+	<-stopCh
+}
+
+func (c *controller) worker() {
+	for c.processNextItem() {
+
+	}
+}
+
+func (c *controller) processNextItem() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+	defer c.queue.Done(item)
+
+	key := item.(string)
+
+	err := c.syncService(key)
+	if err != nil {
+		c.handlerError(key, err)
+		return false
+	}
+	return true
+}
+
+func (c *controller) syncService(key string) error {
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return err
+	}
+
+	// 如果没有 service，不做处理
+	// 注意：如果删除 service，k8s 的处理逻辑会将 OwnerReferences 为该 service 的 CR 删除
+	service, err := c.serviceLister.Services(namespace).Get(name)
+	if errors.IsNotFound(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	// 新增和删除 ingress
+	_, ok := service.GetAnnotations()["ingress/http"]
+	ingress, err := c.ingressLister.Ingresses(namespace).Get(name)
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	if ok && errors.IsNotFound(err) {
+		// create ingress
+		ig := c.constructIngress(service)
+		c.client.NetworkingV1().Ingresses(namespace).Create(context.TODO(), ig, metav1.CreateOptions{})
+
+	} else if !ok && ingress != nil {
+		// delete ingress
+		c.client.NetworkingV1().Ingresses(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *controller) constructIngress(service *corev1.Service) *networkingv1.Ingress {
+	ingress := networkingv1.Ingress{}
+	ingress.Name = service.Name
+	ingress.Namespace = service.Namespace
+	ingress.OwnerReferences = []metav1.OwnerReference{
+		*metav1.NewControllerRef(service, corev1.SchemeGroupVersion.WithKind("Service")),
+	}
+	pathtype := networkingv1.PathTypePrefix
+	ingress.Spec = networkingv1.IngressSpec{
+		Rules: []networkingv1.IngressRule{
+			{
+				Host: "example.com",
+				IngressRuleValue: networkingv1.IngressRuleValue{
+					HTTP: &networkingv1.HTTPIngressRuleValue{
+						Paths: []networkingv1.HTTPIngressPath{
+							{
+								Path:     "/",
+								PathType: &pathtype,
+								Backend: networkingv1.IngressBackend{
+									Service: &networkingv1.IngressServiceBackend{
+										Name: service.Name,
+										Port: networkingv1.ServiceBackendPort{
+											Number: 80,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return &ingress
+}
+
+func (c *controller) handlerError(key string, err error) {
+	if c.queue.NumRequeues(key) <= maxRetry {
+		c.queue.AddRateLimited(key)
+		return
+	}
+
+	log.Fatalln(err)
+	c.queue.Forget(key)
+}
+
+func NewController(client kubernetes.Interface, serviceInformer informer.ServiceInformer, ingressInformer networkinginformer.IngressInformer) controller {
+	c := controller{
+		client:        client,
+		ingressLister: ingressInformer.Lister(),
+		serviceLister: serviceInformer.Lister(),
+		queue:         workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "ingressManager"),
+	}
+
+	serviceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    c.addService,
+		UpdateFunc: c.updateService,
+	})
+
+	ingressInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		DeleteFunc: c.deleteIngress,
+	})
+
+	return c
+}
